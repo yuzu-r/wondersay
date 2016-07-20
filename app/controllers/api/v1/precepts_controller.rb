00796cc580e6492a1678c2260b26648b31b4d0c5
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-07-20 04:18:46 +0000

    don't break when there are no quotes

--- app/controllers/api/v1/precepts_controller.rb
+++ app/controllers/api/v1/precepts_controller.rb
@@ -7,8 +7,12 @@ module Api
     class PreceptsController < JSONAPI::ResourceController
       def random
         quote = Quote.order("RANDOM()").first
-        render :json => {quote: quote.quote, author: quote.author, :callback => params[:callback]}
-        # sometimes listed as params['callback']
+        if quote
+          render :json => {quote: quote.quote, author: quote.author, :callback => params[:callback]}
+          # sometimes listed as params['callback']
+        else
+          render :json => {quote: "nothing here!", author: "nobody", :callback => params[:callback]}
+        end
       end
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Quote.create(quote: "Fortune favors the bold.", author: "Virgil", book: "Wonder")
