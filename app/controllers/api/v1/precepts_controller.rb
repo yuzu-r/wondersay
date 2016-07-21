596fe7c49aef77849e501bfb77911e241189f128
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-07-21 21:28:55 +0000

    try again

--- app/controllers/api/v1/precepts_controller.rb
+++ app/controllers/api/v1/precepts_controller.rb
@@ -5,13 +5,18 @@ end
 module Api
   module V1
     class PreceptsController < JSONAPI::ResourceController
+      respond_to :html, :json
       def random
         quote = Quote.order("RANDOM()").first
         if quote
           #render :json => {quote: quote.quote, author: quote.author, :callback => params[:callback]}
           # sometimes listed as params['callback']
           response = params[:callback] + '(' + quote.to_json + ')'
-          render :text => response
+          #render :text => response
+          respond_to do |format|
+            format.html
+            format.json { render :json => response}
+          end
         else
           render :json => {quote: "nothing here!", author: "nobody", :callback => params[:callback]}
         end
