8c41f5675c0ad3d126809b510a4f38034700ad52
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-07-28 23:32:38 +0000

    quote index

--- config/routes.rb
+++ config/routes.rb
@@ -11,26 +11,17 @@ Rails.application.routes.draw do
       end
     end
   end
-  #get 'users/admin' => 'devise/registrations#index'
+
   devise_scope :user do
     get 'users/admin' => 'users/registrations#index'
     get 'users/access/:id' => 'users/registrations#access', as: 'get_user_access'
     patch 'users/access/:id' => 'users/registrations#access_update'
   end
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
   # Example resource route with options:
   #   resources :products do
